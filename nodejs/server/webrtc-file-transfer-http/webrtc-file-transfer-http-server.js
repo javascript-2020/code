1992f19cfd38717901bf2b1b7d3979d5055c1383
matt <javascript.12.03.2024@gmail.com>
2025-12-02 19:41:52 +0000

    save file

--- nodejs/server/webrtc-file-transfer-http/webrtc-file-transfer-http-server.js
+++ nodejs/server/webrtc-file-transfer-http/webrtc-file-transfer-http-server.js
@@ -3,10 +3,10 @@
 
 /*
 
-//file-transfer-server.js:d
+//webrtc-file-transfer-http-server.js:d
 
 22-07-25
-
+02-12-25
 
 */
                                                                                 console.clear();
@@ -32,10 +32,8 @@
           }
 
 
-
           var host              = argv('h','host')||'127.0.0.1';
           var port              = argv('p','port')||3001;
-
 
           
           var server    = require('https').createServer({key,cert},request).listen({host,port});
@@ -57,10 +55,10 @@
                   case '/test'          : request.test(req,res);          break;
                   
                   case '/init'          : request.init(req,res);          break;
+                  
                   case '/setup'         : request.setup(req,res);         break;
                   case '/offer'         : request.offer(req,res);         break;
                   case '/answer'        : request.answer(req,res);        break;
-                  case '/sdp'           : request.sdp(req,res);           break;
                   case '/ice'           : request.ice(req,res);           break;
                   case '/read'          : request.read(req,res);          break;
                   
@@ -199,26 +197,6 @@
         }//answer
         
         
-        request.sdp   = async function(req,res){
-        
-              var index   = 0;
-              if(req.headers.mode=='polite'){
-                    index   = 1;
-              }
-              
-              var body    = '';
-              for await(data of req)body   += data;
-                                                                  console.log('request.sdp',req.headers.mode,index);
-                                                                  //console.log(body);
-              
-              
-              clients[index].sdp    = body;
-              
-              res.end('ok');
-              
-        }//sdp
-        
-        
         request.ice   = async function(req,res){
         
               var index   = 0;
@@ -265,102 +243,11 @@
 
               res.end(str);
               
-              
-              
-                            
-/*              
-              if(con){              
-
-                                                      
-                    if(con.sdp){
-                          res.write('sdp\n\n');
-                          res.write(con.sdp);
-                          res.write('\n\n');
-                          con.sdp   = null;
-                    }
-                    
-                    if(con.ice.length){
-                          con.ice.forEach(ice=>{
-                          
-                                res.write('ice\n\n');
-                                res.write(ice);
-                                res.write('\n\n');
-                                
-                          });
-                          con.ice.length    = 0;
-                    }
-                    
-              }
-              
-              res.end();
-*/
-
-
         }//read
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-          function upgrade(req,socket,head){
-          
-                var con   = wsmod.upgrade.server(req,socket,onrec,onclose,onerror);
-                
-                clients.push(con);
-
-                
-                var json    = {type:'init'};
-                var mode    = 'polite';
-                if(clients.length==1){
-                      mode    = 'master';
-                }
-                json.mode   = mode;
-                
-                con.send.json(json);
-
-                
-          }//upgrade
-          
-          
-          function onrec(buf,type,con){
-                                                                                //console.log('onrec',type,buf);
-                if(type==='text'){
-                      var str   = buf.toString();
-                      clients.forEach(con2=>{
-                      
-                            if(con2!==con){
-                                  con2.send.text(buf);
-                            }
-                            
-                      });
-                }
-                      
-          }//onrec
-          
-          
-          function onclose(){
-          }//onclose
-          
-          
-          function onerror(){
-          }//onerror
-          
-          
-*/
-
-
+  //:
+  
 
         function argv(id0){
         
@@ -392,25 +279,15 @@
 
 
 
-
-
-
-
-
-
-
-
-
-          
-          function terminal_title(str){
+        function terminal_title(str){
                                                                                 console.log(str);
                                                                                 console.log();
-                var esc   = String.fromCharCode(27);
-                var c7    = String.fromCharCode(7);
-                var cmd   = `${esc}]0;${str}${c7}`;
-                process.stdout.write(cmd);
-                
-          }//terminal_title
+              var esc   = String.fromCharCode(27);
+              var c7    = String.fromCharCode(7);
+              var cmd   = `${esc}]0;${str}${c7}`;
+              process.stdout.write(cmd);
+              
+        }//terminal_title
 
 
 
