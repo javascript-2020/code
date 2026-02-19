a28e6df8ab1a07b29f75881820f7d5bc43dbb0c6
matt <javascript.12.03.2024@gmail.com>
2026-02-19 17:45:16 +0000

    save file

---
+++ nodejs/server/file-server/create-archive.m.js
@@ -0,0 +1,28 @@
+
+
+        export {create_archive};
+        
+        function create_archive(){
+        
+              var {create_archive}    = await import('https://libs.ext-code.com/js/io/create-archive/create-archive.m.js');
+              
+              
+              var zip   = {
+                    'file-server'   :{directory:{
+                          'fiile-server.js'   : {file:{github:{repo:'code',path:'/nodejs/server/file-server/file-server.js'}}},
+                          
+                          'node_modules'        : {directory:{
+                                'getmime.js'    : {file:{github:{repo:'libs',path:'/js/string/getmime/getmime.js'}}},
+                                'keys.js'       : {file:{github:{repo:'libs',path:'/nodejs/keys/keys.js'}}},
+                          }}
+                          
+                    }}
+              };
+              
+              
+              create_archive(zip,{download:'file-server.zip',df:true});
+              
+              
+        }//create_archive
+        
+        
