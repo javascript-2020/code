11ecb609781e3bcc587a9a8218cf9400600848c0
matt <javascript.12.03.2024@gmail.com>
2026-04-21 10:44:56 +0100

    save file

--- nodejs/servers/https-server/https-server.js
+++ nodejs/servers/https-server/https-server.js
@@ -10,7 +10,7 @@
 
 */
 
-      
+
       var fs            = require('fs');
       
       var keys          = require('keys.js');
@@ -18,7 +18,7 @@
       var {key,cert}    = require('server-cert.js');
       var getmime       = require('getmime.js');
       var argv          = require('argv.js');
-
+      
       var root          = __dirname+'/';
       
       var host          = argv('h','host','127.0.0.1');
@@ -59,12 +59,18 @@
                   return;
             }
             
+            var stat    = fs.statSync(abs);
+            if(!stat.isFile()){
+                  badrequest(req,res);
+                  return;
+            }
+            
             var mime    = getmime(abs);
-                                                    
+            
             var buf     = fs.readFileSync(abs);
             res.writeHead(200,{'content-type':mime});
             res.end(buf);
-      
+            
       }//request
       
       
@@ -75,6 +81,13 @@
             
       }//notfound
       
+      
+      function badrequest(req,res){
+      
+            res.writeHead(400);
+            res.end(`${req.url} bad request`);
+            
+      }//badrequest
       
       function cmd(req,res){
       
@@ -92,8 +105,9 @@
             console.log('keypressed',key);
             
       }//keypressed
-
-
-
-
+      
+      
+      
+      
+      
       
